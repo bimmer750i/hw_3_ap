5aeed5ed82f98dfc1724ed1f89d7e241e1416194
bimmer750i <15annmac@gmail.com>
2026-03-14 14:05:09 +0300

    Postgres added

--- app/database.py
+++ app/database.py
@@ -1,18 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # Настройки базы данных
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://admin:adminpassword@db:5432/shortener_db"
+)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=5,
+    max_overflow=10,
+    echo=True
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Функция для получения сессии базы данных
 def get_db():
     db = SessionLocal()
     try:
